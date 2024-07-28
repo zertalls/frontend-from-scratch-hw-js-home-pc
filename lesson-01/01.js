349b4993a0225d0f646f43e7c28e81cb774ee9ee
zertalls <zertalls@mail.ru>
2024-07-28 15:41:03 +0300

    home-re-lesson-2

---
+++ lesson-01/01.js
@@ -0,0 +1,11 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let MAX_COUNTER = 20;
+let sum = 0, currCounter = 1;
+
+while (currCounter <= MAX_COUNTER) {
+    sum +=currCounter;
+    currCounter++;
+}
---
+++ lesson-02/03.js
@@ -0,0 +1,23 @@
+/*
+ * Напишите код, который будет определять оценку студента (grade) в зависимости от балла (score).
+ * Балл может быть целым числом от 0 до 100, а оценки 'F' (0 - 49), 'D' (50 - 69), 'C' (70-79), 'B' (80 - 89), 'A' (90 - 100)
+ * Результат сохраните в переменной grade.
+ */
+
+const score = 70 // тестовое значение, можно изменять
+let grade
+// your code
+
+if (score > 0 && score <= 49) {
+    grade = "F"
+} else if (score <= 69) {
+    grade = "D"
+} else if (score <= 79) {
+    grade = "C"
+} else if (score <= 89) {
+    grade = "B"
+} else if (score <= 100) {
+    grade = "A"
+}
+
+console.log(grade);

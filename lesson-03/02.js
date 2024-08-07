336b965e119a95be4e1ffd9dc5ef5a7a730c7c6d
zertalls <zertalls@mail.ru>
2024-08-07 16:24:09 +0300

    t3.2

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,6 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(num) {    
+    return num % 2 == 0 ? true : false
+}

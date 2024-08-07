498bac3c7ebde99bdacbf3fabc7bea781294f717
zertalls <zertalls@mail.ru>
2024-08-07 16:21:51 +0300

    t3.1

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,5 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(num_1, num_2) {
+    return (num_1 + num_2) / 2
+}

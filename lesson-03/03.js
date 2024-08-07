25db54ae93d28f99cb19eff6ebf7dee41347be65
zertalls <zertalls@mail.ru>
2024-08-07 16:29:34 +0300

    t3.3

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,15 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num_1, num_2, num_3) {
+
+    if (num_1 > num_2 && num_1 > num_3) {
+        return num_1;
+    }
+    else if (num_2 > num_1 && num_2 > num_3) {
+        return num_2;
+    }
+    else {
+        return num_3;
+    }
+
+}

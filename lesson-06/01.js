9352b64100eac6b7ebb52956276be737b5f650c9
zertalls <zertalls@mail.ru>
2024-08-31 20:14:25 +0300

    6.1

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -12,8 +12,38 @@
 🧙‍♂️ Совет: обратите внимание на управление индексом текущего изображения — это ключ к успешному переключению изображений.
 */
 
+let currImageIndex = 0;
+
 const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const img = document.querySelector("#web-tech-image");
+const btnPrev = document.querySelector("#prev-button");
+const btnNext = document.querySelector("#next-button");
+
+btnPrev.addEventListener("click", function() {
+  
+  if (currImageIndex === 0) {
+    currImageIndex = WEB_TECH_IMAGES.length - 1;
+  } else {
+    currImageIndex--;
+  }
+
+  img.setAttribute("src", WEB_TECH_IMAGES[currImageIndex]);
+})
+
+btnNext.addEventListener("click", function() {
+
+  if (currImageIndex === WEB_TECH_IMAGES.length - 1) {
+    currImageIndex = 0;
+  } else {
+    currImageIndex++;
+  }
+
+  img.setAttribute("src", WEB_TECH_IMAGES[currImageIndex]);  
+})
+
+

471c65a966d5bdff12ad7a0e22370c07d55784f6
zertalls <zertalls@mail.ru>
2024-09-20 19:19:37 +0300

    9.2 - 1

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,20 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+// const map = () => {}
+
+const map = (array, callback) => {
+  let result = []  ;
+
+  for (let index = 0; index < array.length; index++) {
+    result.push(callback(array[index], index))    
+  }
+
+  return result;
+}
+
+const callback = (element, index) => {
+  return element * 2;
+}
+
+
